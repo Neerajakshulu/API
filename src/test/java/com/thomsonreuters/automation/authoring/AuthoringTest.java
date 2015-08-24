6c49cc437bfad6665ceadafa6ad804048854848c
Mohana Yalamarthi <mohana.yalamarthi@thomsonreuters.com>
2015-08-24 20:51:16 +0530

    Added authoring tests

---
+++ src/test/java/com/thomsonreuters/automation/authoring/AuthoringTest.java
@@ -0,0 +1,35 @@
+package com.thomsonreuters.automation.authoring;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import com.thomsonreuters.automation.common.AbstractBase;
+
+/**
+ * Test the Authoring APIs
+ */
+public class AuthoringTest extends AbstractBase {
+
+	private static final Logger logger = LogManager.getLogger();
+
+	@BeforeSuite
+	public void beforeSuite() throws Exception {
+		logger.info("@BeforeSuite - any initialization / activity to perform before starting your test suite");
+		strDateTime = new SimpleDateFormat(TESTOUTPUT_FOLDER_DATEFORMAT).format(new Date());
+
+		// This method get all the application host names for the given environment
+		getAllAppHostsForGivenEnv(ENV);
+	}
+
+	@Test
+	public void authoringTest() throws Exception {
+		testDataExcelPath = "src/test/test-data/AuthoringTestData.xlsx";
+		appName = "1PAUTHORING";
+		runTests();
+	}
+}
