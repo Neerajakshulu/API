53feff61b8fbcc6ef4170f744eb3260d2971d0a1
Phani Kumar <phani.kumarb@thomsonreuters.com>
2015-08-24 17:22:20 +0530

    Search API test cases

---
+++ src/test/java/com/thomsonreuters/automation/search/SearchTest.java
@@ -0,0 +1,33 @@
+package com.thomsonreuters.automation.search;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import com.thomsonreuters.automation.common.AbstractBase;
+
+
+public class SearchTest extends AbstractBase {
+
+	private static final Logger logger = LogManager.getLogger();
+	
+	@BeforeSuite
+	public void beforeSuite() throws Exception {
+		logger.info("@BeforeSuite - any initialization / activity to perform before starting your test suite");
+		strDateTime = new SimpleDateFormat(TESTOUTPUT_FOLDER_DATEFORMAT).format(new Date());
+
+		// This method get all the application host names for the given environment
+		getAllAppHostsForGivenEnv(ENV);
+	}
+
+	@Test
+	public void profileTest() throws Exception {
+		testDataExcelPath = "src/test/test-data/SearchTestData.xlsx";
+		appName = "1PCITATIONS";
+		runTests();
+	}
+}
