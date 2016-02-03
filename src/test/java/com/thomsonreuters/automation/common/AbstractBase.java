95700c457bd437935220aed07ea5f56afb4837d3
Avinash Potti <avinash.potti@thomsonreuters.com>
2016-02-03 20:55:15 +0530

    Added test cases in 1PNotify Modified AbstractBase.java

--- src/test/java/com/thomsonreuters/automation/common/AbstractBase.java
+++ src/test/java/com/thomsonreuters/automation/common/AbstractBase.java
@@ -315,7 +315,7 @@ public abstract class AbstractBase {
 						if (appHosts.get(rowData.getHost()) != null) {
 							logger.debug("Real host=" + appHosts.get(rowData.getHost()));
 							if ("1PNOTIFY".equalsIgnoreCase(rowData.getHost())) {
-								Thread.sleep(5000);
+								Thread.sleep(7000);
 							}
 							try{
 								process(row, sheetName);
