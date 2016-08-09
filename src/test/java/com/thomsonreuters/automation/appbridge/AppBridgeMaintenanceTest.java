b1d19f4314a27903322a117a2e64caa712e6cf0d
Avinash Potti <avinash.potti@thomsonreuters.com>
2016-08-09 19:11:01 +0530

    Added scripts for 1PAPPBRIDGE

---
+++ src/test/java/com/thomsonreuters/automation/appbridge/AppBridgeMaintenanceTest.java
@@ -0,0 +1,37 @@
+/*******************************************************************************
+ * Copyright (c) 2012 : Thomson Reuters Global Resources. All Rights Reserved. Proprietary and Confidential information
+ * of TRGR. Disclosure, Use or Reproduction without the written authorization of TRGR is prohibited
+ *
+ ******************************************************************************/
+package com.thomsonreuters.automation.appbridge;
+
+import org.testng.annotations.Test;
+
+import com.thomsonreuters.automation.common.AbstractBase;
+
+/**
+ * The {@code AppBridgeTest} class to test for the Appbridge API's.
+ *
+ * @author Avinash P
+ * 
+ */
+public class AppBridgeMaintenanceTest extends AbstractBase {
+
+	/**
+	 * {@code AppBridgeTest} method is the entry point to test 1PAPPBRIDGE API's.<BR>
+	 * Initializes excel file path and app name. Calls runTests method for executing test cases specified in the
+	 * AppBridgeTestData.xlsx file.</BR>
+	 * 
+	 * @throws Exception On Executing the 1PAPPBRIDGE tests
+	 * @see Exception
+	 * 
+	 */
+
+	@Test
+	public void experimentTest() throws Exception {
+		testDataExcelPath = "src/test/test-data/AppBridgeTestData.xlsx";
+		appName = "1PAPPBRIDGE";
+		runTests("ENDNOTE_MAINTENANCE");
+	}
+
+}
