00ead61fcb7b814b896161f9c3a8d76fffe2d615
janardhanareddy-elluru <janardhan.reddyelluru@thomsonreuters.com>
2017-01-25 18:36:17 +0530

    Added new test cases, images for Media Streaming service for Profile.

--- src/test/java/com/thomsonreuters/automation/media/MediaTest.java
+++ src/test/java/com/thomsonreuters/automation/media/MediaTest.java
@@ -2,9 +2,15 @@ package com.thomsonreuters.automation.media;
 
 import static com.jayway.restassured.RestAssured.given;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.util.Map;
 
 import org.apache.commons.lang.StringUtils;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.jayway.restassured.path.json.JsonPath;
@@ -20,111 +26,85 @@ import com.thomsonreuters.automation.common.RowData;
 public class MediaTest extends AbstractBase {
 
 	@Test(priority = 1)
-	public void profileTest() throws Exception {
+	public void MediaTest() throws Exception {
 		testDataExcelPath = "src/test/test-data/MediaTestData.xlsx";
 		appName = "1PMEDIA";
 		runTests();
 	}
-	
-	
 
-	//@Test
-	public void testSummaryMaxLength() throws Exception {
-		logger.info("Entered Profile testSummaryMaxLength method...");
-		appName = "1PPROFILE";
-		rowData = new RowData();
-		rowData.setTestName("OPQA-497_1");
-		rowData.setHost("1PPROFILE");
-		rowData.setDescription("Verify that update user profile summary with exceeds Max length and verify that API should truncate to 1500 characters");
-		rowData.setApiPath("/users/user/(SYS_USER1)");
-		rowData.setMethod("PUT");
-		rowData.setHeaders("X-1P-User=(SYS_USER1)||Content-Type=application/json");
-		rowData.setBody(
-				"{\"summary\":\"Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with   Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length Summary test with max length\"}");
-		rowData.setValidations("status=200||hits.hits._source.firstName=Mohana");
-		rowData.setStore("hits.hits._id");
-		testReporter=reporter.startTest(rowData.getTestName(), rowData.getDescription()).assignCategory(appName);
-		ExecuteTest(rowData);
-		reporter.endTest(testReporter);
-		logger.info("Entered Profile testSummaryMaxLength method...");
-	}
-
-	private void ExecuteTest(RowData rowData) throws Exception {
+	/**
+	 * {@code getAPIResponce} method to build URL and calling rest assured API's and returns response object
+	 * 
+	 * @return Response json
+	 */
+	@Override
+	protected Response getAPIResponce() {
 		Response response = null;
-		String sheetName = "Profile";
-		String apiPath = null;
-		String headers = null;
-		String queryString = null;
-		String url = null;
-		String responseJson = null;
-		String statusCode = null;
-		boolean testSuccess = false;
+		String apiPath = replaceDynamicPlaceHolders(rowData.getApiPath());
+		String headers = replaceDynamicPlaceHolders(rowData.getHeaders());
+		String queryString = replaceDynamicPlaceHolders(rowData.getQueryString());
+		String bodyString = replaceDynamicPlaceHolders(rowData.getBody());
+		boolean isStreamTest = ((StringUtils.isNotBlank(rowData.getHeaders()))?(rowData.getHeaders().contains("multipart")?true:false):false);
+		logger.debug("isStreamTest=" + isStreamTest);
 
-		logger.debug("row data=" + rowData.toString());
-		logger.debug("Real host=" + appHosts.get(rowData.getHost()));
-		/*
-		 * If mandatory information like test case name, host, api path and
-		 * valid http method are not provided then skip those tests and update
-		 * the status as fail.
-		 */
-		if (StringUtils.isNotBlank(rowData.getTestName()) && StringUtils.isNotBlank(rowData.getHost())
-				&& StringUtils.isNotBlank(rowData.getApiPath()) && isSupportedMethod(rowData.getMethod())) {
-			// If any of the dependency test failed then don't proceed.
-			if (isDependencyTestsPassed(rowData.getDependencyTests())) {
-				logger.info("-----------------------------------------------------------------------");
-				logger.info("Starting test:" + rowData.getTestName());
-				apiPath = replaceDynamicPlaceHolders(rowData.getApiPath());
-				headers = replaceDynamicPlaceHolders(rowData.getHeaders());
-				queryString = replaceDynamicPlaceHolders(rowData.getQueryString());
-				url = appHosts.get(rowData.getHost()) + apiPath + queryString;
-				logger.debug("URL=" + url);
-				RequestSpecification reqSpec = given();
-				// Get and set headers to request
-				if (StringUtils.isNotBlank(rowData.getHeaders())) {
-					Map<String, String> headersMap = getHeaders(headers);
-					reqSpec.headers(headersMap);
-				}
+		String url = appHosts.get(rowData.getHost()) + apiPath + queryString;
+		logger.debug("URL=" + url);
+		testReporter.log(LogStatus.INFO, "Request Method - " + rowData.getMethod());
+		if (StringUtils.isNotBlank(headers))
+			testReporter.log(LogStatus.INFO, "Request Headers - " + headers);
+		if (StringUtils.isNotBlank(bodyString))
+			testReporter.log(LogStatus.INFO, "Request Body - " + bodyString);
+		testReporter.log(LogStatus.INFO, "Request URL - " + url);
 
-				// Set body to request if the http method is not GET.
-				if (!rowData.getMethod().equalsIgnoreCase(GET) && StringUtils.isNotBlank(rowData.getBody())) {
-					reqSpec.body(rowData.getBody());
-				}
+		RequestSpecification reqSpec = given();
 
-				if (rowData.getMethod().equalsIgnoreCase(PUT)) {
-					logger.debug("Entered into PUT Method");
-
-					// Call the Rest API and get the response
-					response = reqSpec.when().put(url);
-
-				}
-
-				// response.then().log().all();
-				responseJson = response.asString();
-				statusCode = String.valueOf(response.getStatusCode());
-				// Validate the response with expected data
-//				testSuccess = testSummaryMaxLengthvalidateResponse(rowData.getValidations(), responseJson, statusCode);
-				if(statusCode.equals("200")){
-				JsonPath jsonPath = new JsonPath(responseJson);
-				String summary = jsonPath.getString("summary");
-				if (summary.length() == 1500) {
-					logger.info("summary text was truncated to 1500 characters");
-					testReporter.log(LogStatus.INFO, "summary text was truncated to 1500 characters");
-					testSuccess = true;
-				}
-				}
-				// Save API response to file
-				saveAPIResponse(responseJson, sheetName, rowData.getTestName());
-
-				if (!testSuccess) {
-					testReporter.log(LogStatus.FAIL, "FAIL");
-					throw new Exception("Validation Failed");
-				}else{
-					testReporter.log(LogStatus.PASS, "PASS" );
-				}
-
-				logger.info("End execution of test:" + rowData.getTestName());
-				logger.info("-----------------------------------------------------------------------");
-			}
+		// Get and set headers to request
+		if (StringUtils.isNotBlank(rowData.getHeaders())) {
+			Map<String, String> headersMap = getHeaders(headers);
+			reqSpec.headers(headersMap);
 		}
+		if (!rowData.getMethod().equalsIgnoreCase(GET) && StringUtils.isNotBlank(bodyString) && !isStreamTest) {
+			reqSpec.body(bodyString);
+		}
+
+		if (rowData.getMethod().equalsIgnoreCase(GET)) {
+			logger.debug("Entered into GET Method");
+
+			// Call the Rest API and get the response
+			response = reqSpec.when().get(url);
+
+		} else if (rowData.getMethod().equalsIgnoreCase(PUT)) {
+			logger.debug("Entered into PUT Method");
+
+			// Call the Rest API and get the response
+			if(isStreamTest){
+				response = reqSpec
+						.multiPart("mediaEntity", new File("src/test/resources/media/updateMediaStreamProfile.json"), "application/json")
+						.multiPart("mediaStream", new File(rowData.getBody()))
+						.when().put(url);
+			}else{
+				response = reqSpec.when().put(url);
+			}
+
+		} else if (rowData.getMethod().equalsIgnoreCase(POST)) {
+			logger.debug("Entered into POST Method");
+
+			// Call the Rest API and get the response
+			if(isStreamTest){
+				response = reqSpec
+						.multiPart("mediaEntity", new File("src/test/resources/media/createMediaStreamProfile.json"), "application/json")
+						.multiPart("mediaStream", new File(rowData.getBody()))
+						.when().post(url);
+			}else{
+				response = reqSpec.when().post(url);
+			}
+
+		} else if (rowData.getMethod().equalsIgnoreCase(DELETE)) {
+			logger.debug("Entered into DELETE Method");
+
+			// Call the Rest API and get the response
+			response = reqSpec.when().delete(url);
+		}
+		return response;
 	}
 }
