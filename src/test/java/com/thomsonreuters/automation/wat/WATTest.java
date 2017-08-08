b9d7e118937c2c95d02e4741da214a2a3d212140
Chinna Putha <chinna.putha@thomsonreuters.com>
2017-08-08 15:39:07 +0530

    WAT app API test cases

---
+++ src/test/java/com/thomsonreuters/automation/wat/WATTest.java
@@ -0,0 +1,32 @@
+package com.thomsonreuters.automation.wat;
+
+import org.testng.annotations.Test;
+
+import com.thomsonreuters.automation.common.AbstractBase;
+
+/**
+ * The {@code FollowTest} class to test for the WAT API's.
+ *
+ * @author Chinna Putha
+ * 
+ */
+
+public class WATTest extends AbstractBase {
+
+	/**
+	 * {@code followTest} method is the entry point to test 1PRecommend(WAT) API's.<BR>
+	 * Initializes excel file path and app name. Calls runTests method for executing test cases specified in the
+	 * FollowTestData.xlsx file.</BR>
+	 * 
+	 * @throws Exception On Executing the WAT  tests
+	 * @see Exception
+	 * 
+	 */
+
+	@Test
+	public void wosAuthTransTest() throws Exception {
+		testDataExcelPath = "src/test/test-data/WATTestData.xlsx";
+		appName = "WAT";
+		runTests();
+	}
+}
